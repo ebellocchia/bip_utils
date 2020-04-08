66943ca205ea02f9ae90319ba4841be171b674e4
Emanuele <ebellocchia@gmail.com>
2020-04-08 15:40:59 +0200

    Version updated after net versions fix.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bip_utils",
-    version="0.4.0",
+    version="0.4.1",
     author="Emanuele Bellocchia",
     author_email="ebellocchia@gmail.com",
     description="Implementation of BIP39, BIP32, BIP44, BIP49 and BIP84 for wallet seeds, keys and addresses generation. Supported coins: Bitcoin, Litecoin, Dogecoin, Ethereum.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ebellocchia/bip_utils",
-    download_url="https://github.com/ebellocchia/bip_utils/archive/v0.4.0.tar.gz",
+    download_url="https://github.com/ebellocchia/bip_utils/archive/v0.4.1.tar.gz",
     license="MIT",
     test_suite="tests",
     install_requires = ["ecdsa","pysha3"],
