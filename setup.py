974d2fecb2c3dce687d2144257a6826505299f31
Emanuele <ebellocchia@gmail.com>
2020-04-08 16:06:05 +0200

    Maintainer, keywords and platforms fields added.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="bip_utils",
     version="0.4.1",
     author="Emanuele Bellocchia",
     author_email="ebellocchia@gmail.com",
+    maintainer="Emanuele Bellocchia",
+    maintainer_email="ebellocchia@gmail.com",
     description="Implementation of BIP39, BIP32, BIP44, BIP49 and BIP84 for wallet seeds, keys and addresses generation. Supported coins: Bitcoin, Litecoin, Dogecoin, Ethereum.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,6 +20,8 @@ setuptools.setup(
     install_requires = ["ecdsa","pysha3"],
     packages=["bip_utils"],
     package_data={"bip_utils": ["bip39_wordslist_en.txt"]},
+    keywords="bitcoin, litecoin, dogecoin, dash, ethereum, ripple, wallet, hd-wallet, bip39, bip32, bip44, bip49, bip84, python",
+    platforms = ["any"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
