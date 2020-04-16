ac794cb90896cbfd6a49d3528ce4f574582f2bc2
Emanuele <ebellocchia@gmail.com>
2020-04-16 21:08:33 +0200

    Add new imports from new exception files.

--- bip_utils/__init__.py
+++ bip_utils/__init__.py
@@ -1,23 +1,30 @@
 # Version
 from ._version      import __version__
-# Utility libraries
-from .base58        import Base58Decoder, Base58Encoder, Base58ChecksumError
+# Base58
+from .base58_ex     import Base58ChecksumError
+from .base58        import Base58Decoder, Base58Encoder
+# Bech32
+from .bech32_ex     import Bech32ChecksumError, Bech32FormatError
 from .bech32        import Bech32Decoder, Bech32Encoder, Bech32ChecksumError, Bech32FormatError
+# WIF
 from .wif           import WifDecoder, WifEncoder
+# Address computation
 from .P2PKH         import P2PKH
 from .P2SH          import P2SH
 from .P2WPKH        import P2WPKH
 from .eth_addr      import EthAddr
 from .xrp_addr      import XrpAddr
 # BIP39
-from .bip39         import EntropyGenerator, Bip39MnemonicGenerator, Bip39MnemonicValidator, Bip39SeedGenerator, Bip39ChecksumError
+from .bip39_ex      import Bip39InvalidFileError, Bip39ChecksumError
+from .bip39         import EntropyGenerator, Bip39MnemonicGenerator, Bip39MnemonicValidator, Bip39SeedGenerator
 # BIP32
 from .bip32_ex      import Bip32KeyError, Bip32PathError
 from .bip32_utils   import Bip32Utils
 from .bip32_path    import Bip32PathParser
 from .bip32         import Bip32
 # BIP44/49/84
-from .bip44_base    import Bip44Changes, Bip44Coins, Bip44DepthError
+from .bip44_base_ex import Bip44DepthError, Bip44CoinNotAllowedError
+from .bip44_base    import Bip44Changes, Bip44Coins, Bip44Levels
 from .bip44         import Bip44
 from .bip49         import Bip49
 from .bip84         import Bip84
