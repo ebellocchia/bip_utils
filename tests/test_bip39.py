f4cf531152fe5a65da413c3c770b4119b42a7483
Emanuele <ebellocchia@gmail.com>
2020-04-21 10:52:46 +0200

    Add missing tests from https://github.com/trezor/python-mnemonic/blob/master/vectors.json

--- tests/test_bip39.py
+++ tests/test_bip39.py
@@ -29,6 +29,7 @@ from bip_utils import EntropyGenerator, Bip39MnemonicGenerator, Bip39MnemonicVal
 # https://github.com/trezor/python-mnemonic/blob/master/vectors.json
 TEST_MAIN = \
     [
+        # Basic 12-words
         {
             "entropy"  : b"00000000000000000000000000000000",
             "mnemonic" :  "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about",
@@ -49,6 +50,7 @@ TEST_MAIN = \
             "mnemonic" :  "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong",
             "seed"     : b"ac27495480225222079d7be181583751e86f571027b0497b5b5d11218e0a8a13332572917f0f8e5a589620c6f15b11c61dee327651a14c34e18231052e48c069",
         },
+        # Basic 18-words
         {
             "entropy"  : b"000000000000000000000000000000000000000000000000",
             "mnemonic" :  "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon agent",
@@ -69,15 +71,90 @@ TEST_MAIN = \
             "mnemonic" :  "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo when",
             "seed"     : b"0cd6e5d827bb62eb8fc1e262254223817fd068a74b5b449cc2f667c3f1f985a76379b43348d952e2265b4cd129090758b3e3c2c49103b5051aac2eaeb890a528",
         },
+        # Basic 24-words
+        {
+            "entropy"  : b"0000000000000000000000000000000000000000000000000000000000000000",
+            "mnemonic" :  "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art",
+            "seed"     : b"bda85446c68413707090a52022edd26a1c9462295029f2e60cd7c4f2bbd3097170af7a4d73245cafa9c3cca8d561a7c3de6f5d4a10be8ed2a5e608d68f92fcc8",
+        },
+        {
+            "entropy"  : b"7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
+            "mnemonic" :  "legal winner thank year wave sausage worth useful legal winner thank year wave sausage worth useful legal winner thank year wave sausage worth title",
+            "seed"     : b"bc09fca1804f7e69da93c2f2028eb238c227f2e9dda30cd63699232578480a4021b146ad717fbb7e451ce9eb835f43620bf5c514db0f8add49f5d121449d3e87",
+        },
+        {
+            "entropy"  : b"8080808080808080808080808080808080808080808080808080808080808080",
+            "mnemonic" :  "letter advice cage absurd amount doctor acoustic avoid letter advice cage absurd amount doctor acoustic avoid letter advice cage absurd amount doctor acoustic bless",
+            "seed"     : b"c0c519bd0e91a2ed54357d9d1ebef6f5af218a153624cf4f2da911a0ed8f7a09e2ef61af0aca007096df430022f7a2b6fb91661a9589097069720d015e4e982f",
+        },
+        {
+            "entropy"  : b"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+            "mnemonic" :  "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote",
+            "seed"     : b"dd48c104698c30cfe2b6142103248622fb7bb0ff692eebb00089b32d22484e1613912f0a5b694407be899ffd31ed3992c456cdf60f5d4564b8ba3f05a69890ad",
+        },
+        # Various
+        {
+            "entropy"  : b"9e885d952ad362caeb4efe34a8e91bd2",
+            "mnemonic" :  "ozone drill grab fiber curtain grace pudding thank cruise elder eight picnic",
+            "seed"     : b"274ddc525802f7c828d8ef7ddbcdc5304e87ac3535913611fbbfa986d0c9e5476c91689f9c8a54fd55bd38606aa6a8595ad213d4c9c9f9aca3fb217069a41028",
+        },
+        {
+            "entropy"  : b"6610b25967cdcca9d59875f5cb50b0ea75433311869e930b",
+            "mnemonic" :  "gravity machine north sort system female filter attitude volume fold club stay feature office ecology stable narrow fog",
+            "seed"     : b"628c3827a8823298ee685db84f55caa34b5cc195a778e52d45f59bcf75aba68e4d7590e101dc414bc1bbd5737666fbbef35d1f1903953b66624f910feef245ac",
+        },
+        {
+            "entropy"  : b"68a79eaca2324873eacc50cb9c6eca8cc68ea5d936f98787c60c7ebc74e6ce7c",
+            "mnemonic" :  "hamster diagram private dutch cause delay private meat slide toddler razor book happy fancy gospel tennis maple dilemma loan word shrug inflict delay length",
+            "seed"     : b"64c87cde7e12ecf6704ab95bb1408bef047c22db4cc7491c4271d170a1b213d20b385bc1588d9c7b38f1b39d415665b8a9030c9ec653d75e65f847d8fc1fc440",
+        },
+
+        {
+            "entropy"  : b"c0ba5a8e914111210f2bd131f3d5e08d",
+            "mnemonic" :   "scheme spot photo card baby mountain device kick cradle pact join borrow",
+            "seed"     : b"ea725895aaae8d4c1cf682c1bfd2d358d52ed9f0f0591131b559e2724bb234fca05aa9c02c57407e04ee9dc3b454aa63fbff483a8b11de949624b9f1831a9612",
+        },
         {
             "entropy"  : b"6d9be1ee6ebd27a258115aad99b7317b9c8d28b6d76431c3",
             "mnemonic" :  "horn tenant knee talent sponsor spell gate clip pulse soap slush warm silver nephew swap uncle crack brave",
             "seed"     : b"fd579828af3da1d32544ce4db5c73d53fc8acc4ddb1e3b251a31179cdb71e853c56d2fcb11aed39898ce6c34b10b5382772db8796e52837b54468aeb312cfc3d",
         },
         {
+            "entropy"  : b"9f6a2878b2520799a44ef18bc7df394e7061a224d2c33cd015b157d746869863",
+            "mnemonic" :  "panda eyebrow bullet gorilla call smoke muffin taste mesh discover soft ostrich alcohol speed nation flash devote level hobby quick inner drive ghost inside",
+            "seed"     : b"72be8e052fc4919d2adf28d5306b5474b0069df35b02303de8c1729c9538dbb6fc2d731d5f832193cd9fb6aeecbc469594a70e3dd50811b5067f3b88b28c3e8d",
+        },
+
+        {
+            "entropy"  : b"23db8160a31d3e0dca3688ed941adbf3",
+            "mnemonic" :   "cat swing flag economy stadium alone churn speed unique patch report train",
+            "seed"     : b"deb5f45449e615feff5640f2e49f933ff51895de3b4381832b3139941c57b59205a42480c52175b6efcffaa58a2503887c1e8b363a707256bdd2b587b46541f5",
+        },
+        {
+            "entropy"  : b"8197a4a47f0425faeaa69deebc05ca29c0a5b5cc76ceacc0",
+            "mnemonic" :  "light rule cinnamon wrap drastic word pride squirrel upgrade then income fatal apart sustain crack supply proud access",
+            "seed"     : b"4cbdff1ca2db800fd61cae72a57475fdc6bab03e441fd63f96dabd1f183ef5b782925f00105f318309a7e9c3ea6967c7801e46c8a58082674c860a37b93eda02",
+        },
+        {
+            "entropy"  : b"066dca1a2bb7e8a1db2832148ce9933eea0f3ac9548d793112d9a95c9407efad",
+            "mnemonic" :  "all hour make first leader extend hole alien behind guard gospel lava path output census museum junior mass reopen famous sing advance salt reform",
+            "seed"     : b"26e975ec644423f4a4c4f4215ef09b4bd7ef924e85d1d17c4cf3f136c2863cf6df0a475045652c57eb5fb41513ca2a2d67722b77e954b4b3fc11f7590449191d",
+        },
+
+        {
+            "entropy"  : b"f30f8c1da665478f49b001d94c5fc452",
+            "mnemonic" :   "vessel ladder alter error federal sibling chat ability sun glass valve picture",
+            "seed"     : b"2aaa9242daafcee6aa9d7269f17d4efe271e1b9a529178d7dc139cd18747090bf9d60295d0ce74309a78852a9caadf0af48aae1c6253839624076224374bc63f",
+        },
+        {
             "entropy"  : b"c10ec20dc3cd9f652c7fac2f1230f7a3c828389a14392f05",
             "mnemonic" :  "scissors invite lock maple supreme raw rapid void congress muscle digital elegant little brisk hair mango congress clump",
             "seed"     : b"7b4a10be9d98e6cba265566db7f136718e1398c71cb581e1b2f464cac1ceedf4f3e274dc270003c670ad8d02c4558b2f8e39edea2775c9e232c7cb798b069e88",
+        },
+        {
+            "entropy"  : b"f585c11aec520db57dd353c69554b21a89b20fb0650966fa0a9d6f74fd989d8f",
+            "mnemonic" :  "void come effort suffer camp survey warrior heavy shoot primary clutch crush open amazing screen patrol group space point ten exist slush involve unfold",
+            "seed"     : b"01f5bced59dec48e362f2c45b5de68b9fd6c92c6634f44d6d40aab69056506f0e35524a518034ddc1192e1dacd32c1ed3eaa3c3b131c88ed8e7e54c49a5d0998",
         },
     ]
 
