255649b0848058e825b033c0f2b4b34a1d6fea52
Emanuele <ebellocchia@gmail.com>
2020-08-07 15:15:42 +0200

    Add unit tests for Atom address and Bech32

---
+++ tests/test_atom_addr.py
@@ -0,0 +1,77 @@
+# Copyright (c) 2020 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Imports
+import binascii
+import unittest
+from bip_utils import AtomAddr
+
+# Some keys randomly taken (verified with Cosmostation wallet)
+TEST_VECT = \
+    [
+        {
+            "pub_key" : b"039cb22e5c6ce15e06b76d5725dcf084b87357d926dcdfeeb20d628d3d11ff543b",
+            "hrp"     :  "cosmos",
+            "address" :  "cosmos1zewfm2c4s6uv5s4rywksqden8dvya4wmqyyvek",
+        },
+        {
+            "pub_key" : b"02dc27af24c0fc6b448519e17d4ac6078f158a766bbf8446cb16c61a9e53835c3c",
+            "hrp"     :  "cosmos",
+            "address" :  "cosmos1n6ugmlarydek7k8wslzuy55seftfe7g2aqncw3",
+        },
+        {
+            "pub_key" : b"0356ab0a0717738c794caf972ee2091762525a35d062c881b863733f06f445c585",
+            "hrp"     :  "band",
+            "address" :  "band16nez6ldt0zp648zgk8g2af50245y0ykjutc2k9",
+        },
+        {
+            "pub_key" : b"02b19f4692195f95a8d919edf245d64993bce60bb3c50e4226ba5311686ccf60da",
+            "hrp"     :  "band",
+            "address" :  "band16a3pvl8jmf84uvreek79mta5jr8llmcn4ptgy2",
+        },
+    ]
+
+# Tests for invalid keys
+TEST_VECT_KEY_INVALID = \
+    [
+        # Private key (not accepted by Atom address)
+        b"132750b8489385430d8bfa3871ade97da7f5d5ef134a5c85184f88743b526e38",
+        # Uncompressed public key (not accepted by Atom address)
+        b"b19f4692195f95a8d919edf245d64993bce60bb3c50e4226ba5311686ccf60dae29953a4250eae34b7c4dd8c9e31d798ad597d5725e81767f2eeb515307ee5f2"
+        # Compressed public key with invalid length
+        b"029efbcb2db9ee44cb12739e9350e19e5f1ce4563351b770096f0e408f93400c7000",
+    ]
+
+
+#
+# Tests
+#
+class AtomAddrTests(unittest.TestCase):
+    # Run all tests in test vector
+    def test_to_addr(self):
+        for test in TEST_VECT:
+            # Decompress key
+            self.assertEqual(test["address"], AtomAddr.ToAddress(binascii.unhexlify(test["pub_key"]), test["hrp"]))
+
+    # Test invalid keys
+    def test_invalid_keys(self):
+        for test in TEST_VECT_KEY_INVALID:
+            self.assertRaises(ValueError, AtomAddr.ToAddress, binascii.unhexlify(test), "cosmos")
---
+++ tests/test_atom_bech32.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2020 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Imports
+import binascii
+import unittest
+from bip_utils import (
+    AtomBech32Decoder, AtomBech32Encoder, Bech32ChecksumError, Bech32FormatError, AtomBech32FormatError
+)
+
+# Some keys randomly taken from Ian Coleman web page
+# https://iancoleman.io/bip39/
+TEST_VECT = \
+    [
+        {
+            "raw"    : b"751e76e8199196d454941c45d1b3a323f1433bd6",
+            "encode" :  "cosmos1w508d6qejxtdg4y5r3zarvary0c5xw7k6ah60c",
+        },
+        {
+            "raw"    : b"30ea99599334801bf09d753af38ba546800bea8b",
+            "encode" :  "cosmos1xr4fjkvnxjqphuyaw5a08za9g6qqh65t36srck",
+        },
+        {
+            "raw"    : b"18abaed50b7c1176308baa094b054383b775f12c",
+            "encode" :  "band1rz46a4gt0sghvvyt4gy5kp2rswmhtufv49nfef",
+        },
+        {
+            "raw"    : b"29595a3c78760fe90fe883b922f353b67441d28d",
+            "encode" :  "band199v450rcwc87jrlgswuj9u6nke6yr55dxjrx4e",
+        },
+    ]
+
+# Tests for BCH encoded addresses
+# Few tests because most cases are already covered by Segwit tests
+TEST_VECT_ADDR_INVALID = \
+    [
+        #
+        # From BIP-0173 page
+        #
+
+        # Invalid human-readable part
+        {
+            "addr": "cosmis1w508d6qejxtdg4y5r3zarvary0c5xw7khxen85",
+            "hrp" : "cosmos",
+            "ex"  : AtomBech32FormatError,
+        },
+        # Invalid program length
+        {
+            "addr": "cosmos1w554mng0",
+            "hrp" : "cosmos",
+            "ex"  : AtomBech32FormatError,
+        },
+        # Invalid program length
+        {
+            "addr": "cosmos1w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kqq2z39yp",
+            "hrp" : "cosmos",
+            "ex"  : AtomBech32FormatError,
+        },
+    ]
+
+
+#
+# Tests
+#
+class AtomBech32Tests(unittest.TestCase):
+    # Test decoder
+    def test_decoder(self):
+        for test in TEST_VECT:
+            # Test decoder
+            hrp = test["encode"][:test["encode"].find("1")]
+            dec = AtomBech32Decoder.Decode(hrp, test["encode"])
+            self.assertEqual(binascii.hexlify(dec), test["raw"])
+
+    # Test encoder
+    def test_encoder(self):
+        for test in TEST_VECT:
+            # Test encoder
+            hrp = test["encode"][:test["encode"].find("1")]
+            enc = AtomBech32Encoder.Encode(hrp, binascii.unhexlify(test["raw"]))
+            self.assertEqual(test["encode"], enc)
+
+    # Test invalid address
+    def test_invalid_addr(self):
+        for test in TEST_VECT_ADDR_INVALID:
+            print(test["addr"])
+            self.assertRaises(test["ex"],  AtomBech32Decoder.Decode, test["hrp"], test["addr"])
