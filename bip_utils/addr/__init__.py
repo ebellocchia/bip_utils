a4c640ae64106caaf1c995a8c08381275e19ec38
Emanuele <ebellocchia@gmail.com>
2020-08-15 11:02:35 +0200

    * Rename P2PKH, P2SH and P2WPKH files by adding "_addr" suffix Rename P2PKH, P2SH and P2WPKH files to be compliant with the other address computation files Update related imports in __init__.py

--- bip_utils/addr/__init__.py
+++ bip_utils/addr/__init__.py
@@ -1,7 +1,7 @@
-from bip_utils.addr.P2PKH     import P2PKH, BchP2PKH
-from bip_utils.addr.P2SH      import P2SH, BchP2SH
-from bip_utils.addr.P2WPKH    import P2WPKH
-from bip_utils.addr.atom_addr import AtomAddr
-from bip_utils.addr.eth_addr  import EthAddr
-from bip_utils.addr.trx_addr  import TrxAddr
-from bip_utils.addr.xrp_addr  import XrpAddr
+from bip_utils.addr.P2PKH_addr  import P2PKH, BchP2PKH
+from bip_utils.addr.P2SH_addr   import P2SH, BchP2SH
+from bip_utils.addr.P2WPKH_addr import P2WPKH
+from bip_utils.addr.atom_addr   import AtomAddr
+from bip_utils.addr.eth_addr    import EthAddr
+from bip_utils.addr.trx_addr    import TrxAddr
+from bip_utils.addr.xrp_addr    import XrpAddr
