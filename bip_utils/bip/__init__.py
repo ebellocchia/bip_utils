d6d7fa441f23106589e9251aee3b6bcd3f5605e5
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-04-23 21:09:06 +0200

    Remove useless spaces from __init__.py

--- bip_utils/bip/__init__.py
+++ bip_utils/bip/__init__.py
@@ -1,17 +1,17 @@
 # BIP39
 from bip_utils.bip.bip39_ex import Bip39InvalidFileError, Bip39ChecksumError
-from bip_utils.bip.bip39    import (
+from bip_utils.bip.bip39 import (
     Bip39WordsNum, Bip39EntropyBitLen,
     Bip39EntropyGenerator, Bip39MnemonicGenerator, Bip39MnemonicValidator, Bip39SeedGenerator
 )
 # BIP32
-from bip_utils.bip.bip32_ex      import Bip32KeyError, Bip32PathError
-from bip_utils.bip.bip32_utils   import Bip32Utils
-from bip_utils.bip.bip32_path    import Bip32PathParser
-from bip_utils.bip.bip32         import Bip32
+from bip_utils.bip.bip32_ex import Bip32KeyError, Bip32PathError
+from bip_utils.bip.bip32_utils import Bip32Utils
+from bip_utils.bip.bip32_path import Bip32PathParser
+from bip_utils.bip.bip32 import Bip32
 # BIP44/49/84
 from bip_utils.bip.bip44_base_ex import Bip44DepthError, Bip44CoinNotAllowedError
-from bip_utils.bip.bip44_base    import Bip44Changes, Bip44Coins, Bip44Levels
-from bip_utils.bip.bip44         import Bip44
-from bip_utils.bip.bip49         import Bip49
-from bip_utils.bip.bip84         import Bip84
+from bip_utils.bip.bip44_base import Bip44Changes, Bip44Coins, Bip44Levels
+from bip_utils.bip.bip44 import Bip44
+from bip_utils.bip.bip49 import Bip49
+from bip_utils.bip.bip84 import Bip84
--- bip_utils/conf/__init__.py
+++ bip_utils/conf/__init__.py
@@ -1,4 +1,4 @@
 from bip_utils.conf.bip_coin_conf import *
-from bip_utils.conf.bip44_coins   import *
-from bip_utils.conf.bip49_coins   import *
-from bip_utils.conf.bip84_coins   import *
+from bip_utils.conf.bip44_coins import *
+from bip_utils.conf.bip49_coins import *
+from bip_utils.conf.bip84_coins import *
