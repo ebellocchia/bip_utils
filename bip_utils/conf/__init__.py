a24ac4669ef47c8098c3d69b716e597dd425a505
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-04-25 14:27:55 +0200

    Add conf helper import

--- bip_utils/conf/__init__.py
+++ bip_utils/conf/__init__.py
@@ -1,4 +1,5 @@
 from bip_utils.conf.bip_coin_conf import *
+from bip_utils.conf.bip_coin_conf_helper import *
 from bip_utils.conf.bip44_coins import *
 from bip_utils.conf.bip49_coins import *
 from bip_utils.conf.bip84_coins import *
