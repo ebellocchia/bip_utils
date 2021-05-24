ab2a45c774bc4aa2560828d87b6a595793a787d3
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-05-24 10:34:41 +0200

    Delete KeyUtils import

--- bip_utils/utils/__init__.py
+++ bip_utils/utils/__init__.py
@@ -2,4 +2,3 @@ from bip_utils.utils.algo import AlgoUtils
 from bip_utils.utils.bit import BitUtils
 from bip_utils.utils.conversion import ConvUtils
 from bip_utils.utils.crypto import CryptoUtils
-from bip_utils.utils.key import KeyUtils
