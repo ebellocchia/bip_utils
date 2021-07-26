81019e6d06793ccb37a53d8fbff5fe297dea9cfb
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-07-26 13:53:44 +0200

    Add address encoding for Monero

---
+++ bip_utils/addr/xmr_addr.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2021 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Imports
+from typing import Union
+from bip_utils.addr.utils import AddrUtils
+from bip_utils.base58 import Base58XmrEncoder
+from bip_utils.ecc import IPublicKey
+from bip_utils.utils import CryptoUtils
+
+
+class XmrAddrConst:
+    """ Class container for Monero address constants. """
+
+    # Checksum length in bytes
+    CHECKSUM_BYTE_LEN: int = 4
+
+
+class XmrAddr:
+    """ Monero address class. It allows the Monero address generation. """
+
+    @staticmethod
+    def EncodeKey(pub_spend_key: Union[bytes, IPublicKey],
+                  pub_view_key: Union[bytes, IPublicKey],
+                  net_ver: bytes) -> str:
+        """ Get address in Monero format.
+
+        Args:
+            pub_spend_key (bytes or IPublicKey): Spend public key bytes or object
+            pub_view_key (bytes or IPublicKey) : View public key bytes or object
+            net_ver (bytes)                    : Net version
+
+        Returns:
+            str: Address string
+
+        Raises:
+            ValueError: If the public key is not valid
+            TypeError: If the public key is not ed25519-monero
+        """
+        pub_spend_key_obj = AddrUtils.ValidateAndGetEd25519MoneroKey(pub_spend_key)
+        pub_view_key_obj = AddrUtils.ValidateAndGetEd25519MoneroKey(pub_view_key)
+
+        data = net_ver + pub_spend_key_obj.RawCompressed().ToBytes()[1:] + pub_view_key_obj.RawCompressed().ToBytes()[1:]
+        checksum = CryptoUtils.Kekkak256(data)
+
+        return Base58XmrEncoder.Encode(data + checksum[:XmrAddrConst.CHECKSUM_BYTE_LEN])
