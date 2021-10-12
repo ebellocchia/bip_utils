80b95d7afc96eb9ff8ba0f55f9424f5a8f07c6de
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-10-12 11:35:50 +0200

    Export substrate configuration

--- bip_utils/substrate/conf/__init__.py
+++ bip_utils/substrate/conf/__init__.py
@@ -1,3 +1,4 @@
 from bip_utils.substrate.conf.substrate_coins import SubstrateCoins
 from bip_utils.substrate.conf.substrate_coin_conf import SubstrateCoinConf
+from bip_utils.substrate.conf.substrate_conf import *
 from bip_utils.substrate.conf.substrate_conf_getter import SubstrateConfGetter
