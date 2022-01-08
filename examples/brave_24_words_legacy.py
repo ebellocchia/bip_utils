e8f1c69807bff3092ab9407a63a8916a22412b8d
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2022-01-08 20:55:29 +0100

    Update examples

---
+++ examples/brave_24_words_legacy.py
@@ -0,0 +1,23 @@
+"""
+Example of key derivation with 24-words mnemonic phrase in the same way of Brave crypto wallets extension (legacy).
+
+The old version of Brave crypto wallets extension (now referred as "legacy") worked differently from BIP39 when a 24-words mnemonic phrase was used.
+In fact, instead of computing the seed from the mnemonic as described in BIP39, it used the initial entropy directly as seed.
+Therefore, the derived keys and addresses were completely different to other wallets.
+"""
+
+from bip_utils import Bip39MnemonicDecoder, Bip44Changes, Bip44Coins, Bip44
+
+# Mnemonic
+mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art"
+
+# Get back entropy bytes from mnemonic
+entropy_bytes = Bip39MnemonicDecoder().Decode(mnemonic)
+
+# Use the entropy bytes directly as seed
+bip44_mst_ctx = Bip44.FromSeed(entropy_bytes, Bip44Coins.ETHEREUM)
+
+# Derive the key of the first address
+bip44_addr_ctx = bip44_mst_ctx.Purpose().Coin().Account(0).Change(Bip44Changes.CHAIN_EXT).AddressIndex(0)
+# Same address of Brave crypto wallets extension
+print(f"Address: {bip44_addr_ctx.PublicKey().ToAddress()}")
--- examples/exodus_ed25519.py
+++ examples/exodus_ed25519.py
@@ -1,5 +1,5 @@
 """
-Example of key derivation for ed25519-based coins like the Exodus wallet.
+Example of key derivation for ed25519-based coins in the same way of Exodus wallet.
 
 Basically, the Exodus wallet uses the secp256k1 curve to derive the complete BIP44 path for all coins, even if they are not based on the secp256k1 curve.
 Then, for the coins based on the ed25519 curve (e.g. Algorand, Solana, Stellar, ...), it uses the last derived private key as a ed25519 master key to compute the public key and address.
@@ -28,4 +28,4 @@ priv_key_bytes = bip32_ctx.PrivateKey().Raw().ToBytes()
 # Use the last private key as a ed25519 master key, we can use Bip44 to simplify the address computation
 bip44_ctx = Bip44.FromPrivateKey(priv_key_bytes, coin_type)
 # Same address of Exodus
-print(bip44_ctx.PublicKey().ToAddress())
+print(f"Address: {bip44_ctx.PublicKey().ToAddress()}")
