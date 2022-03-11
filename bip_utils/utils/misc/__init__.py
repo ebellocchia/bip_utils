ffdc9913885098139e6fc668f92b407197cdbf71
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2022-03-11 09:03:10 +0100

    Add Sha256 class

--- bip_utils/utils/misc/__init__.py
+++ bip_utils/utils/misc/__init__.py
@@ -5,4 +5,5 @@ from bip_utils.utils.misc.bytes import BytesUtils
 from bip_utils.utils.misc.crypto import CryptoUtils
 from bip_utils.utils.misc.data_bytes import DataBytes
 from bip_utils.utils.misc.integer import IntegerUtils
+from bip_utils.utils.misc.sha256 import Sha256
 from bip_utils.utils.misc.string import StringUtils
