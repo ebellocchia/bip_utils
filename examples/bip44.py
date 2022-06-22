6700b22e13e9385f002df7f65efabf778e5e35d9
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2022-06-22 21:38:47 +0200

    Update examples

--- examples/bip44.py
+++ examples/bip44.py
@@ -4,6 +4,8 @@ from bip_utils import (
     Bip39WordsNum, Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44Changes, Bip44Coins, Bip44
 )
 
+ADDR_NUM: int = 5
+
 # Generate random mnemonic
 mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
 print(f"Mnemonic string: {mnemonic}")
@@ -22,9 +24,10 @@ bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0)
 # Generate BIP44 chain keys: m/44'/0'/0'/0
 bip44_chg_ctx = bip44_acc_ctx.Change(Bip44Changes.CHAIN_EXT)
 
-# Generate the first 10 addresses: m/44'/0'/0'/0/i
-for i in range(10):
+# Generate addresses: m/44'/0'/0'/0/i
+print("Addresses:")
+for i in range(ADDR_NUM):
     bip44_addr_ctx = bip44_chg_ctx.AddressIndex(i)
-    print(f"{i}. Address public key (extended): {bip44_addr_ctx.PublicKey().ToExtended()}")
-    print(f"{i}. Address private key (extended): {bip44_addr_ctx.PrivateKey().ToExtended()}")
-    print(f"{i}. Address: {bip44_addr_ctx.PublicKey().ToAddress()}")
+    print(f"  {i}. Address public key (extended): {bip44_addr_ctx.PublicKey().ToExtended()}")
+    print(f"  {i}. Address private key (extended): {bip44_addr_ctx.PrivateKey().ToExtended()}")
+    print(f"  {i}. Address: {bip44_addr_ctx.PublicKey().ToAddress()}")
--- examples/electrum.py
+++ examples/electrum.py
@@ -1,46 +1,80 @@
 """Example of mnemonic generation and key derivation like the Electrum wallet."""
 
 from bip_utils import (
-    Bip32Secp256k1,
+    Bip32PrivateKey, Bip32Secp256k1,
     CoinsConf,
+    ElectrumV1WordsNum,
+    ElectrumV1MnemonicGenerator, ElectrumV1SeedGenerator,
+    ElectrumV1Wallet,
     ElectrumV2WordsNum, ElectrumV2MnemonicTypes,
     ElectrumV2MnemonicGenerator, ElectrumV2SeedGenerator,
     ElectrumV2WalletStandard, ElectrumV2WalletSegwit,
-    WifEncoder
+    IPrivateKey, WifPubKeyModes, WifEncoder
 )
 
+
+ADDR_NUM: int = 5
+
+
+# Encode private key to WIF
+def priv_to_wif(priv_key: IPrivateKey,
+                pub_key_mode: WifPubKeyModes = WifPubKeyModes.COMPRESSED) -> str:
+    return WifEncoder.Encode(priv_key,
+                             CoinsConf.BitcoinMainNet.Params("wif_net_ver"),
+                             pub_key_mode)
+
+
+print("V1 Wallet")
 # Generate random standard mnemonic
-standard_mnemonic = ElectrumV2MnemonicGenerator(ElectrumV2MnemonicTypes.STANDARD).FromWordsNumber(ElectrumV2WordsNum.WORDS_NUM_12)
-print(f"Standard mnemonic: {standard_mnemonic}")
-# Generate seed from mnemonic
-standard_seed_bytes = ElectrumV2SeedGenerator(standard_mnemonic).Generate()
-
+v1_mnemonic = ElectrumV1MnemonicGenerator().FromWordsNumber(ElectrumV1WordsNum.WORDS_NUM_12)
+print(f"Mnemonic: {v1_mnemonic}")
+v1_seed_bytes = ElectrumV1SeedGenerator(v1_mnemonic).Generate()
 # Construct from seed
-electrum_standard = ElectrumV2WalletStandard(
-    Bip32Secp256k1.FromSeed(standard_seed_bytes)
-)
-# Derive the first 5 standard addresses
-for i in range(5):
-    priv_key_wif = WifEncoder.Encode(electrum_standard.GetPrivateKey(0, i).KeyObject(),
-                                     CoinsConf.BitcoinMainNet.Params("wif_net_ver"))
-    print(f"{i}. Standard private key: {priv_key_wif}")
-    print(f"{i}. Standard address: {electrum_standard.GetAddress(0, i)}")
+electrum_v1 = ElectrumV1Wallet(v1_seed_bytes)
+# Print master key
+print(f"Master private key: {priv_to_wif(electrum_v1.MasterPrivateKey(), WifPubKeyModes.UNCOMPRESSED)}")
+# Derive V1 addresses
+print("Addresses:")
+for i in range(ADDR_NUM):
+    print(f"  {i}. Private key: {priv_to_wif(electrum_v1.GetPrivateKey(0, i), WifPubKeyModes.UNCOMPRESSED)}")
+    print(f"  {i}. Address: {electrum_v1.GetAddress(0, i)}")
 
 print("")
-
-# Generate random segwit mnemonic
-segwit_mnemonic = ElectrumV2MnemonicGenerator(ElectrumV2MnemonicTypes.SEGWIT).FromWordsNumber(ElectrumV2WordsNum.WORDS_NUM_12)
-print(f"Segwit mnemonic: {segwit_mnemonic}")
+print("V2 Wallet (standard)")
+# Generate random standard mnemonic
+v2_standard_mnemonic = ElectrumV2MnemonicGenerator(ElectrumV2MnemonicTypes.STANDARD).FromWordsNumber(ElectrumV2WordsNum.WORDS_NUM_12)
+print(f"Mnemonic: {v2_standard_mnemonic}")
 # Generate seed from mnemonic
-segwit_seed_bytes = ElectrumV2SeedGenerator(segwit_mnemonic).Generate()
+v2_standard_seed_bytes = ElectrumV2SeedGenerator(v2_standard_mnemonic).Generate()
 
 # Construct from seed
-electrum_segwit = ElectrumV2WalletSegwit(
-    Bip32Secp256k1.FromSeed(segwit_seed_bytes)
+electrum_v2_standard = ElectrumV2WalletStandard(
+    Bip32Secp256k1.FromSeed(v2_standard_seed_bytes)
 )
-# Derive the first 5 segwit addresses
-for i in range(5):
-    priv_key_wif = WifEncoder.Encode(electrum_segwit.GetPrivateKey(0, i).KeyObject(),
-                                     CoinsConf.BitcoinMainNet.Params("wif_net_ver"))
-    print(f"{i}. Segwit private key: {priv_key_wif}")
-    print(f"{i}. Segwit address: {electrum_segwit.GetAddress(0, i)}")
+# Print master key
+print(f"Master private key: {priv_to_wif(electrum_v2_standard.MasterPrivateKey().KeyObject())}")
+# Derive standard addresses
+print("Addresses:")
+for i in range(ADDR_NUM):
+    print(f"  {i}. Private key: {priv_to_wif(electrum_v2_standard.GetPrivateKey(0, i).KeyObject())}")
+    print(f"  {i}. Address: {electrum_v2_standard.GetAddress(0, i)}")
+
+print("")
+print("V2 Wallet (segwit)")
+# Generate random segwit mnemonic
+v2_segwit_mnemonic = ElectrumV2MnemonicGenerator(ElectrumV2MnemonicTypes.SEGWIT).FromWordsNumber(ElectrumV2WordsNum.WORDS_NUM_12)
+print(f"Mnemonic: {v2_segwit_mnemonic}")
+# Generate seed from mnemonic
+v2_segwit_seed_bytes = ElectrumV2SeedGenerator(v2_segwit_mnemonic).Generate()
+
+# Construct from seed
+electrum_v2_segwit = ElectrumV2WalletSegwit(
+    Bip32Secp256k1.FromSeed(v2_segwit_seed_bytes)
+)
+# Print master key
+print(f"Master private key: {priv_to_wif(electrum_v2_segwit.MasterPrivateKey().KeyObject())}")
+# Derive segwit addresses
+print("Addresses:")
+for i in range(ADDR_NUM):
+    print(f"  {i}. Private key: {priv_to_wif(electrum_v2_segwit.GetPrivateKey(0, i).KeyObject())}")
+    print(f"  {i}. Address: {electrum_v2_segwit.GetAddress(0, i)}")
