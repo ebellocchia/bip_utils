807ac01ec13347bfd753e31e2f18451078eea68a
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2022-06-22 22:42:47 +0200

    Add tests for Electrum wallet

---
+++ tests/electrum/test_electrum_v1_wallet.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2022 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Imports
+import binascii
+import unittest
+from bip_utils import (
+    CoinsConf, ElectrumV1Wallet, Secp256k1PublicKey, Secp256k1PrivateKey,
+    WifPubKeyModes, WifEncoder
+)
+
+# Test vector (verified with Electrum wallet)
+TEST_VECT = [
+    {
+        "seed": b"0bbe2537d7527f2d7376d4bb9de8ac42ca202dbae310471b88f2cbb0492e6e73",
+        "master_pub_key": "2e767c36bd48f70bc34704cabd5bb1394f374227c330bb17ed6b22ca44b85582d2d88a00fcf8f0f00b2ee32f8623b7d23def43c8c0784846b98c2abaf742693b",
+        "master_priv_key": "5HuTXx6TC56nonxHfw3DmM72CurZ22zh24azdCmz3gh3We2Ujvk",
+        "addresses": [
+            {
+                "address": "1P5Ai2wW2x93onQW5ZSDfHhu5LTgBPNx7r",
+                "priv_key": "5K9JkKyEJ7yoF5dyZBHHQHyXTUPirhxwDYSFn4RV25bN6pXfhLu",
+            },
+            {
+                "address": "1dGnYwstcGq5fsEkSkV3jpgbfKQf2wgR1",
+                "priv_key": "5Ja4ec5Sy2CqA4hRJFkbfJGa3wkhsYyH2ewBbJkXQoK4GtRqp94",
+            },
+            {
+                "address": "1CcNXYJW7DEtqLECPQGoTr8mhZEC7X9i8X",
+                "priv_key": "5KJMwfQEhKSL4Pa6zY3f44tRTXxsSBFBfGtdTe9n3e6DA8YqquS",
+            },
+            {
+                "address": "12Gq5N1tDRUx7Tn7nEgHunazoAcDK1p6sw",
+                "priv_key": "5KkKGuZgFTB5LCxcv9TiUtLEYSj9cYPeZ6xrK34YkpQpdKRdmkL",
+            },
+            {
+                "address": "1L828t7SY3qzpKf3u3LfNNVvUHKtcQxBk8",
+                "priv_key": "5JEUjCFHNgkA5LHtWD6fKeCGEuyo4gNvqgbjNdYDRB3RYHGzgUC",
+            },
+        ],
+    },
+    {
+        "seed": b"d274ee3d2f0ca429e9b4cff307bcdc81ab503adc838b552123a1cf7d908a275b",
+        "master_pub_key": "1bc0ddce2ac75a2d624316ad40d4031d710a16423830dc16d0b3423ee1fa946af8ce5987f6dba0d0078d9c3193373d01456abfd7f65b6c95d42ef3a24a5fba3d",
+        "master_priv_key": "5KQyR8zmHmhEKjkHDpSFEPUKzaeFocywbpsjMN2rxDrRsdtSpDV",
+        "addresses": [
+            {
+                "address": "18W9NHvXtxvs8macnQXTWbPb9nZtd98ydD",
+                "priv_key": "5K6m9kA5ezpaR6tknSZWUirJ2x3bT5UovGRjYpX1bXB1yPKpM7G",
+            },
+            {
+                "address": "1AswJdBRHVkKyBDWRDdrq3TQBD8JepFkxz",
+                "priv_key": "5JU93eDB9HPGScrAVg2HV1G3c8MAwM9qKV8V86LgaLNQFS8Nsk8",
+            },
+            {
+                "address": "14QJ1CFM9FfeWWs8f5XxUtWdngvShpTDHJ",
+                "priv_key": "5JDh1pduCcxmkX1NbuFH9SPoyNRKTFR9FW7NH5yUPHhKGppKWD5",
+            },
+            {
+                "address": "198qDpuez1ez26jx3YEcKG3HLbJfQGs7oS",
+                "priv_key": "5JdQiFK7Az8Yhqrof3aDfu5WnPzv1vHAvMtJaCgjVqHcGrCtTp3",
+            },
+            {
+                "address": "19csUz4nzBrJk8xq4yos3tS545H5u5SbrQ",
+                "priv_key": "5JkcxTjBvmz4nbtT1xGNXGqvZoXaMU2zn292HEptXsYYKt2XKoZ",
+            },
+        ],
+    },
+]
+
+
+#
+# Tests
+#
+class ElectrumV1WalletTests(unittest.TestCase):
+    # Run all tests in test vector
+    def test_vector(self):
+        for test in TEST_VECT:
+            electrum_v1_wallet = ElectrumV1Wallet(binascii.unhexlify(test["seed"]))
+
+            self.assertTrue(isinstance(electrum_v1_wallet.MasterPublicKey(), Secp256k1PublicKey))
+            self.assertTrue(isinstance(electrum_v1_wallet.MasterPrivateKey(), Secp256k1PrivateKey))
+
+            self.assertEqual(test["master_pub_key"], electrum_v1_wallet.MasterPublicKey().RawUncompressed().ToHex()[2:])
+            self.assertEqual(test["master_priv_key"], self.__priv_to_wif(electrum_v1_wallet.MasterPrivateKey()))
+
+            for i, test_addr in enumerate(test["addresses"]):
+                self.assertTrue(isinstance(electrum_v1_wallet.GetPublicKey(0, i), Secp256k1PublicKey))
+                self.assertTrue(isinstance(electrum_v1_wallet.GetPrivateKey(0, i), Secp256k1PrivateKey))
+
+                self.assertEqual(test_addr["address"], electrum_v1_wallet.GetAddress(0, i))
+                self.assertEqual(test_addr["priv_key"], self.__priv_to_wif(electrum_v1_wallet.GetPrivateKey(0, i)))
+
+    # Encode private key to WIF
+    @staticmethod
+    def __priv_to_wif(priv_key):
+        return WifEncoder.Encode(priv_key,
+                                 CoinsConf.BitcoinMainNet.Params("wif_net_ver"),
+                                 WifPubKeyModes.UNCOMPRESSED)
