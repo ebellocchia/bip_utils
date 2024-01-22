0e9870da648dbed945dd7307a0fb8789ba1c12da
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2024-01-22 22:47:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -163,6 +163,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
