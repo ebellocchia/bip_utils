dfa398c5b1f8c0038b307ec5cd323fb3837af24c
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2024-02-12 11:56:45 +0100

    Fix style

--- bip_utils/ecc/ed25519_blake2b/ed25519_blake2b_const.py
+++ bip_utils/ecc/ed25519_blake2b/ed25519_blake2b_const.py
@@ -20,9 +20,8 @@
 
 """Module for ed25519-blake2b constants."""
 
-from bip_utils.ecc.common.ipoint import IPoint
-
 # Imports
+from bip_utils.ecc.common.ipoint import IPoint
 from bip_utils.ecc.ed25519.ed25519 import Ed25519Const
 
 
