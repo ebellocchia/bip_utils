021cf8b2a51ee4bdc5ac90f00319f919199e36bc
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2024-05-04 12:02:21 +0200

    Fix example

--- examples/bip32_slip10_nist256p1.py
+++ examples/bip32_slip10_nist256p1.py
@@ -1,7 +1,7 @@
 """Example of keys derivation using BIP32 (nist256p1 curve)."""
 
 from bip_utils import (
-    Bip32Slip10Nist256p1, Bip39MnemonicGenerator, Bip39SeedGenerator, Bip39WordsNum, CoinsConf, NeoAddr
+    Bip32Slip10Nist256p1, Bip39MnemonicGenerator, Bip39SeedGenerator, Bip39WordsNum, CoinsConf, NeoLegacyAddrEncoder
 )
 
 
@@ -27,6 +27,6 @@ print(f"Derived public key (extended): {bip32_der_ctx.PublicKey().ToExtended()}"
 
 # Print address in NEO encoding
 # The BIP32 elliptic curve shall be the same one expected by NEO (nist256p1 in this case)
-neo_addr = NeoAddr.EncodeKey(bip32_der_ctx.PublicKey().KeyObject(),
-                             ver=CoinsConf.Neo.ParamByKey("addr_ver"))
+neo_addr = NeoLegacyAddrEncoder.EncodeKey(bip32_der_ctx.PublicKey().KeyObject(),
+                                          ver=CoinsConf.Neo.ParamByKey("addr_ver"))
 print(f"Address (NEO): {neo_addr}")
