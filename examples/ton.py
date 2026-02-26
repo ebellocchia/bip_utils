e0018d181647cddc2f8340ce8ca0a3811effb1da
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2026-02-26 16:10:36 +0100

    Update TON example

--- examples/ton.py
+++ examples/ton.py
@@ -1,56 +1,55 @@
 """Example of mnemonic generation and key derivation for TON."""
 
 from bip_utils import (
-    Bip32Ed25519Slip, Bip39SeedGenerator, Bip44, Bip44Coins, Bip44ConfGetter,
-    TonSeedGenerator, TonMnemonicGenerator, TonMnemonicValidator, Ton, TonAddrVersions
+    Bip32Ed25519Slip,
+    Bip39MnemonicGenerator,
+    Bip39SeedGenerator,
+    Bip39WordsNum,
+    Bip44,
+    Bip44Coins,
+    Bip44ConfGetter,
+    TonAddrEncoder,
+    TonAddrVersions,
 )
 
-# Generate mnemonic for Ton wallets (e.g. Tonkeeper)
-mnemonic = TonMnemonicGenerator().FromWordsNumber(24)
-print(f"Mnemonic: {mnemonic}")
 
-# Validate mnemonic
-is_valid = TonMnemonicValidator().IsValid(mnemonic)
-print(f"Is the mnemonic valid? {is_valid}")
+#
+# Generation like Tonkeeper, Tonwallet, Trustwallet
+#
 
-# Generate seed and address from memonic
-seed_bytes = TonSeedGenerator(mnemonic).Generate()
-
-# Generate v5r1 address
-addr = Ton.FromSeed(seed_bytes).GetAddress(TonAddrVersions.V5R1)
-print(f"V5R1 Address: {addr}")
-
-# Generate v4 address
-addr_v4 = Ton.FromSeed(seed_bytes).GetAddress(TonAddrVersions.V4)
-print(f"V4 Address: {addr_v4}")
-
-
-# Generate addresses based on bip44 (e.g. Trustwallet or Ledger)
-
-# Mnemonic
-mnemonic = "bachelor neither fall observe flee give sniff rebel access maximum property beach"
+# Generate random mnemonic
+mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
+print(f"Mnemonic string: {mnemonic}")
 # Generate seed from mnemonic
 seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
 
-coin_type = Bip44Coins.TON
+# Construct from seed
+bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.TON)
+bip44_def_ctx = bip44_mst_ctx.DeriveDefaultPath()
 
-# Get address using the Trustwallet derivation path
-bip44_mst = Bip44.FromSeed(seed_bytes, coin_type)
-bip44_acc = bip44_mst.Purpose().Coin().Account(0)
-addr = bip44_acc.PublicKey().ToAddress()
-print(f"Address using Trustwallet derivation path: {addr}")
+# Print keys
+print(f"Ton private key: {bip44_def_ctx.PrivateKey().Raw().ToHex()}")
+print(f"Ton public key: {bip44_def_ctx.PublicKey().RawCompressed().ToHex()[1:]}")
+# Print address (default version is V4)
+print(f"Ton address (V4): {bip44_def_ctx.PublicKey().ToAddress()}")
+# Print address with other versions
+pub_key_obj = bip44_def_ctx.PublicKey().Bip32Key().KeyObject()
+print(f"Ton address (V3R1): {TonAddrEncoder.EncodeKey(pub_key_obj, version=TonAddrVersions.V3R1)}")
+print(f"Ton address (V3R2): {TonAddrEncoder.EncodeKey(pub_key_obj, version=TonAddrVersions.V3R2)}")
+print(f"Ton address (V5R1): {TonAddrEncoder.EncodeKey(pub_key_obj, version=TonAddrVersions.V5R1)}")
 
-# Ledger uses a non-standard derivation path, so we need to use Bip32 directly
+#
+# Generation like Ledger
+#
 
 # Get coin index from configuration
-coin_idx = Bip44ConfGetter.GetConfig(coin_type).CoinIndex()
-# Account index
-account_idx = 0
-derivation_path = f"m/44'/{coin_idx}'/0'/0'/{account_idx}'/0'"
-
+coin_idx = Bip44ConfGetter.GetConfig(Bip44Coins.TON).CoinIndex()
+# Derive
+derivation_path  = f"m/44'/{coin_idx}'/0'/0'/0'/0'"
 bip32_ctx = Bip32Ed25519Slip.FromSeed(seed_bytes).DerivePath(derivation_path)
-priv_key_bytes = bip32_ctx.PrivateKey().Raw().ToBytes()
 
-bip44_ctx = Bip44.FromPrivateKey(priv_key_bytes, coin_type)
-addr = bip44_ctx.PublicKey().ToAddress()
-print(f"Address using Ledger derivation path: {addr}")
+# Construct BIP44 object from private key
+priv_key_bytes = bip32_ctx.PrivateKey().Raw().ToBytes()
+bip44_ctx = Bip44.FromPrivateKey(priv_key_bytes, Bip44Coins.TON)
+# Print address
+print(f"Ton address (Ledger, V4): {bip44_ctx.PublicKey().ToAddress()}")
