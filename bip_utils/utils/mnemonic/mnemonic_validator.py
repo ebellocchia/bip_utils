5f30d61b2a8f005e3d301474251208e658b6b805
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2026-02-27 16:27:00 +0100

    Fix docstring

--- bip_utils/utils/mnemonic/mnemonic_validator.py
+++ bip_utils/utils/mnemonic/mnemonic_validator.py
@@ -46,7 +46,7 @@ class MnemonicValidator:
     def Validate(self,
                  mnemonic: Union[str, Mnemonic]) -> None:
         """
-        Validate the mnemonic specified at construction.
+        Validate the specified mnemonic.
 
         Args:
             mnemonic (str or Mnemonic object): Mnemonic
@@ -62,7 +62,7 @@ class MnemonicValidator:
     def IsValid(self,
                 mnemonic: Union[str, Mnemonic]) -> bool:
         """
-        Get if the mnemonic specified at construction is valid.
+        Get if the specified mnemonic is valid.
 
         Args:
             mnemonic (str or Mnemonic object): Mnemonic
